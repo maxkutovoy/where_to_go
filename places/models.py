7175128ef0edfd9c9248d9341b367f79cb6a27fe
max <maxkutovoy@yandex.ru>
2022-02-23 16:59:05 +0600

    Add Place model

---
+++ places/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+class Place(models.Model):
+    title = models.CharField('Название', max_length=200)
+    description_short = models.TextField('Короткое описание', blank=True)
+    description_long = models.TextField('Полное описание', blank=True)
+    lat = models.FloatField("Широта")
+    lon = models.FloatField("Долгота")
+
+    def __str__(self):
+        return f"{self.title}"
+
--- where_to_go/urls.py
+++ where_to_go/urls.py
@@ -14,12 +14,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.urls import include
 from django.urls import path
 
+import places
 from where_to_go import views
+
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index)
+    path('', views.index),
+    path('places/', include('places.urls')),
+
 ]
