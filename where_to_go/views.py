ea58f349b7e34e8dbda2d64331517c8fb2c9f7ae
max <maxkutovoy@yandex.ru>
2022-02-25 15:27:15 +0600

    Move data from template to view

--- where_to_go/views.py
+++ where_to_go/views.py
@@ -1,5 +1,34 @@
+import json
+
 from django.shortcuts import render
+from places.models import Place, Image
+
+from pprint import  pprint
 
 
 def index(request):
-    return render(request, 'index.html')
+    places_points = []
+    places = Place.objects.all()
+    for place in places:
+
+        places_points.append(
+            {
+                "type": "Feature",
+                "geometry": {
+                    "type": "Point",
+                    "coordinates": [place.lon, place.lat]
+                },
+                "properties": {
+                    "title": place.title,
+                    "placeId": place.pk,
+                    "detailsUrl": "../static/places/moscow_legends.json"
+                }
+            }
+        )
+
+    features = {
+        "type": "FeatureCollection",
+        "features": places_points
+    }
+
+    return render(request, 'index.html', context=features)
