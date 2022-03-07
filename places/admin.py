70f21485e95c6eb9803d878a38e9047de9b9a199
max <maxkutovoy@yandex.ru>
2022-03-07 22:15:46 +0600

    Add image previews to locations

--- places/admin.py
+++ places/admin.py
@@ -1,10 +1,18 @@
 from django.contrib import admin
+from django.utils.html import format_html
+
 from .models import Place, Image
 
 
 class ImageInLine(admin.TabularInline):
     model = Image
     extra = 1
+    readonly_fields = ("get_preview", )
+
+    def get_preview(self, obj):
+        return format_html(
+            f'<img src="{obj.image.url}" width="200" />'
+        )
 
 
 @admin.register(Place)
@@ -14,4 +22,13 @@ class PlaceAdmin(admin.ModelAdmin):
     ]
 
 
-admin.site.register(Image)
+@admin.register(Image)
+class ImageAdmin(admin.ModelAdmin):
+    list_display = ["__str__", 'image', 'get_preview']
+    fields = ["image_order", 'image', 'get_preview']
+    readonly_fields = ("get_preview",)
+
+    def get_preview(self, obj):
+        return format_html(
+            f'<img src="{obj.image.url}" width="200" />'
+        )
