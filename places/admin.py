cdc7227d53326e50cafd4a0a79904d3fd9047018
max <maxkutovoy@yandex.ru>
2022-03-07 18:13:18 +0600

    Add images to location

--- places/admin.py
+++ places/admin.py
@@ -1,6 +1,17 @@
 from django.contrib import admin
 from .models import Place, Image
 
-# Register your models here.
-admin.site.register(Place)
+
+class ImageInLine(admin.TabularInline):
+    model = Image
+    extra = 1
+
+
+@admin.register(Place)
+class PlaceAdmin(admin.ModelAdmin):
+    inlines = [
+        ImageInLine,
+    ]
+
+
 admin.site.register(Image)
